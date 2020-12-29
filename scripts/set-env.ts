82e72074414d9e62aae47f4dc70fc1b0ddc1e533
Dharmen Shah <dharmen.s@milestoneinternet.com>
2020-12-29 19:54:37 +0530

    fix(set-env.ts): make changes in environment.ts file only

--- scripts/set-env.ts
+++ scripts/set-env.ts
@@ -10,9 +10,7 @@ require('dotenv').config();
 const environment = process.env.NODE_ENV;
 const isProd = environment === 'production';
 
-const targetPath = `./apps/client/src/environments/environment${
-  environment ? '.' + environment : ''
-}.ts`;
+const targetPath = `./apps/client/src/environments/environment.ts`;
 const envConfigFile = `export const environment = {
   production: ${isProd},
   api_url: '${process.env.BACKEND_API_URL}'
