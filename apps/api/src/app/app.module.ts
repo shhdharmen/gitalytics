e0bbc6407fc9321851315df568f83b75f68c1525
shhdharmen <shhdharmen@gmail.com>
2021-04-17 22:04:06 +0530

    build: remove netlify and add heroku

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validationSchema } from './config/validation';
 
 @Module({
-  imports: [HttpModule, ConfigModule.forRoot({ validationSchema })],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot({ validationSchema }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api*'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
