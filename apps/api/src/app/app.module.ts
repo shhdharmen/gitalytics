0b49065c11cf9f11a702f5f46ef124643e144780
shhdharmen <shhdharmen@gmail.com>
2021-04-17 22:29:59 +0530

    feat(api): remove static site

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,10 +11,10 @@ import { validationSchema } from './config/validation';
   imports: [
     HttpModule,
     ConfigModule.forRoot({ validationSchema }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
-      exclude: ['/api*'],
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'client'),
+    //   exclude: ['/api*'],
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
